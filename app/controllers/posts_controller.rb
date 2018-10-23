4eec522c5c3990bea0d0e55a174534fc958e50e1
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-23 09:52:10 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
-  def index	  def index
-    @posts = Post.all	    @posts = Post.all
-@@ -20,4 +21,14 @@ def create
-  end	  end
+  def index
+    @posts = Post.all
+  end
    # add edit and update methods here	  # add edit and update methods here
   def edit
     @post = Post.find(params[:id])
   end
-  
+
   def update
     @post = Post.find(params[:id])
     @post.update(:title => params[:title], description: params[:description])
